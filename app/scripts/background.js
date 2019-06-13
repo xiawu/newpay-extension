ba240206eee4544174e1b686ebb9db4989eb273b
kumavis <aaron@kumavis.me>
2019-06-13 17:29:10 +0800

    i18n - temporarily disable i18n preferred lang check due to weird error

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -159,7 +159,8 @@ setupMetamaskMeshMetrics()
  */
 async function initialize () {
   const initState = await loadStateFromPersistence()
-  const initLangCode = await getFirstPreferredLangCode()
+  // const initLangCode = await getFirstPreferredLangCode()
+  const initLangCode = 'ko'
   await setupController(initState, initLangCode)
   log.debug('MetaMask initialization complete.')
 }
